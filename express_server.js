bbd57e5fcb9850f6c45ca62836511801c06a6e56
Tom Spencer <thomas.spencer2034@gmail.com>
2023-09-29 12:07:53 -0400

    Created users objects db

--- express_server.js
+++ express_server.js
@@ -16,6 +16,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//Database of users
+const users = {
+   userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 app.use(express.urlencoded({ extended: true }));
 
 //Generate a Random Short URL ID
