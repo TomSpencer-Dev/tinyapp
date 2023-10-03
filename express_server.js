3ed1a4a0ebb8a96a2850d0acd129e3f5b4126768
Tom Spencer <thomas.spencer2034@gmail.com>
2023-10-03 10:19:30 -0400

    Modified Get urls/new endpoint to redirect to /login if user is not logged in.

--- express_server.js
+++ express_server.js
@@ -88,7 +88,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.cookies["user_id"]] };
-  res.render("urls_new", templateVars);
+  if (templateVars.user === undefined) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", templateVars);
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
