8a8416e4f894a4b54525c77bccdfdd8546ecedd9
Tom Spencer <thomas.spencer2034@gmail.com>
2023-10-05 16:14:04 -0400

    Added assert statement in describe getUserByEmail test.

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail } = require('../helper.js');
 
 const testUsers = {
   "userRandomID": {
@@ -19,6 +19,6 @@ describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-    // Write your assert statement here
+    assert.equal(user.id, expectedUserID)
   });
 });
