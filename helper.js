ab909aba37f1696e25becf01b708be1dd056035c
Tom Spencer <thomas.spencer2034@gmail.com>
2023-10-06 06:19:25 -0400

    Updated comments in helper.js file.

--- helper.js
+++ helper.js
@@ -1,3 +1,6 @@
+//Module to contain helper functions
+
+//Function to check if user's email exists in the users database
 const getUserByEmail = function(email, database) {
   for (let user in database) {
     if (database[user].email === email) {
@@ -7,4 +10,5 @@ const getUserByEmail = function(email, database) {
   return undefined;
 };
 
+//Export function
 module.exports = {getUserByEmail};
